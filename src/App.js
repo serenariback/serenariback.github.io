539d931622987a35c37c65e9ef1df7ef98b0cf7c
serribac1 <86022165+serribac1@users.noreply.github.com>
2023-06-28 11:39:01 -0400

    site in progress

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Site in progress...
+          Site in progress... come back later
         </p>
       </header>
     </div>
