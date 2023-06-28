168e861d2a8127dc1777156417fcbe315c28fd3f
serribac1 <86022165+serribac1@users.noreply.github.com>
2023-06-28 16:15:40 -0400

    creating folders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
+import { HomeScreen } from './screens/HomeScreen';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Site in progress... come back later
-        </p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Site in progress... come back later
+    //     </p>
+    //   </header>
+    // </div>
+    <HomeScreen />
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css'
+
+export const HomeScreen = () => {
+    return (
+        <div className='title'>
+            Hello. I am Serena
+        </div>
+    )
+};
